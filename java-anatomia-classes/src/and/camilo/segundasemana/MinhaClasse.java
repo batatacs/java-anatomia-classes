d3af91e991bfe7c1ee773b71dda5ee92afaa1f0b
anderson camilo <anderson_cs345@hotmail.com>
2023-03-09 13:02:49 -0300

    primeiro comite

---
+++ java-anatomia-classes/src/and/camilo/segundasemana/MinhaClasse.java
@@ -0,0 +1,18 @@
+package and.camilo.segundasemana;
+
+public class MinhaClasse {
+    
+    public static void main (String [] args) {
+
+        String primeiroNome = "Anderson";
+        String segundoNome = "Camilo";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
